adb8a9f5d6ca9d25df402d9be79091da56f2838e
Frans Budi Kashira <fransbudikashira@gmail.com>
2023-05-28 11:49:42 +0700

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductCategoryController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
+    Route::name('dashboard.')->prefix('dashboard')->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('index');
+        Route::middleware(['admin'])->group(function () {
+            Route::resource('category', ProductCategoryController::class);
+        });
+    });
+});
